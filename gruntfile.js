fffe1831c953bfd2cc82e4fbed51dbf9ce5c2ebe
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-08-11 11:35:52 -0300

    :contruction: :sparkles: Função Sortear Numero

--- gruntfile.js
+++ gruntfile.js
@@ -16,8 +16,82 @@ module.exports = function (grunt) {
         },
       },
     },
+    watch: {
+      less: {
+        files: ["src/styles/**/*.less"],
+        tasks: ["less:development"],
+      },
+      html: {
+        files: ["src/index.html"],
+        tasks: ["replace:dev"],
+      },
+    },
+    replace: {
+      dev: {
+        options: {
+          patterns: [
+            {
+              match: "ENDERECO_DO_CSS",
+              replacement: "./styles/main.css",
+            },
+            {
+              match: "ENDERECO_DO_JS",
+              replacement: "../src/scripts/main.js",
+            },
+          ],
+        },
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ["src/index.html"],
+            dest: "dev/",
+          },
+        ],
+      },
+      dist: {
+        options: {
+          patterns: [
+            {
+              match: "ENDERECO_DO_CSS",
+              replacement: "./styles/main.min.css",
+            },
+          ],
+        },
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ["prebuild/index.html"],
+            dest: "dist/",
+          },
+        ],
+      },
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
+        },
+        files: {
+          "prebuild/index.html": "src/index.html",
+        },
+      },
+    },
+    clean: ["prebuild"],
   });
+
   grunt.loadNpmTasks("grunt-contrib-less");
-  grunt.registerTask("default", ["less:development"]);
-  grunt.registerTask("build", "less:production");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-replace");
+  grunt.loadNpmTasks("grunt-contrib-htmlmin");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.registerTask("default", ["watch"]);
+  grunt.registerTask("build", [
+    "less:production",
+    "htmlmin:dist",
+    "replace:dist",
+    "clean",
+  ]);
 };
