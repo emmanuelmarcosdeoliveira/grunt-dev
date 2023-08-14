65c14197e980ba8d408dd1f9786fd9b2b6ec7e6e
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-08-14 11:19:41 -0300

    :rocket: Pronto para build

--- gruntfile.js
+++ gruntfile.js
@@ -56,6 +56,10 @@ module.exports = function (grunt) {
               match: "ENDERECO_DO_CSS",
               replacement: "./styles/main.min.css",
             },
+            {
+              match: "ENDERECO_DO_JS",
+              replacement: "./scripts/main.min.js",
+            },
           ],
         },
         files: [
@@ -80,6 +84,13 @@ module.exports = function (grunt) {
       },
     },
     clean: ["prebuild"],
+    uglify: {
+      target: {
+        files: {
+          "dist/scripts/main.min.js": "src/scripts/main.js",
+        },
+      },
+    },
   });
 
   grunt.loadNpmTasks("grunt-contrib-less");
@@ -87,11 +98,13 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-replace");
   grunt.loadNpmTasks("grunt-contrib-htmlmin");
   grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.registerTask("default", ["watch"]);
   grunt.registerTask("build", [
     "less:production",
     "htmlmin:dist",
     "replace:dist",
     "clean",
+    "uglify",
   ]);
 };
